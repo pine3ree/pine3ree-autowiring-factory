9f257221eaf4aecc268d47cb6a4357c936aef7c7
pine3ree <pine3ree@gmail.com>
2025-04-24 01:03:28 +0200

    Update crelection-based factory code to match closely the params-resolver
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/App/Container/ReflectionBasedFactory.php
+++ src/App/Container/ReflectionBasedFactory.php
@@ -8,12 +8,18 @@
 
 namespace App\Container;
 
+use App\Container\ParamsResolver;
+use Psr\Container\ContainerInterface;
 use ReflectionClass;
 use ReflectionMethod;
+use ReflectionNamedType;
 use RuntimeException;
-use Psr\Container\ContainerInterface;
+use Throwable;
 
-use function sprintf;
+use function class_exists;
+use function get_class;
+use function interface_exists;
+//use function sprintf;
 
 /**
  * A generic factory that resolves and injects dependencies using reflection
@@ -28,30 +34,66 @@ class ReflectionBasedFactory
 
     public function __invoke(ContainerInterface $container, string $fqcn): object
     {
+        $has_constructor = method_exists($fqcn, '__construct');
+
+        if (!$has_constructor) {
+            return new $fqcn();
+        }
+
+        $paramsResolver
+            = $container->has(ParamsResolver::class)
+            ? $container->get(ParamsResolver::class)
+            : new ParamsResolver($container);
+
+        $args = $paramsResolver->resolve([$fqcn, '__construct']);
+
+        return empty($args) ? new $fqcn() : new $fqcn(...$args);
+
         $rc = $this->getReflectionClass($fqcn);
-        $params = $this->constructorParams[$fqcn];
+        $__r_params = $this->constructorParams[$fqcn];
 
         // Parameter-less object constructor or consructor-less object
-        if (empty($params)) {
+        if (empty($__r_params)) {
             return new $fqcn();
         }
 
         $args = [];
-        foreach ($params as $param) {
-            $prc = $param->getClass();
-            if ($prc instanceof ReflectionClass) {
-                $dep_fqcn = $prc->getName();
-                $args[] = $dep_fqcn === ContainerInterface::class
-                    ? $container
-                    : $container->get($dep_fqcn);
-            } elseif ($param->isDefaultValueAvailable()) {
-                $args[] = $param->getDefaultValue();
+        foreach ($__r_params as $rp) {
+            $rp_name = $rp->getName();
+            $rp_type = $rp->getType();
+            if ($rp_type instanceof ReflectionNamedType && !$rp_type->isBuiltin()) {
+                $rp_fqcn = $rp_type->getName();
+                if (interface_exists($rp_fqcn, true) || class_exists($rp_fqcn, true)) {
+                    if ($rp_fqcn === ContainerInterface::class || $rp_fqcn === get_class($container)) {
+                        $args[] = $container;
+                    } elseif ($container->has($rp_fqcn)) {
+                        $args[] = $container->get($rp_fqcn);
+                    } else {
+                        // Try instantating with argument-less constructor call
+                        try {
+                            $args[] = new $rp_fqcn();
+                        } catch (Throwable $ex) {
+                            throw new RuntimeException(
+                                "Unable to create an instance of `{$rp_fqcn}`"
+                                . " for the argument with name `{$rp_name}`!",
+                            );
+                        }
+                    }
+                } else {
+                    throw new RuntimeException(
+                        "`{$rp_fqcn}` is neither a valid interface nor a class name"
+                        . " for given dependency named `{$rp_name}`!",
+                    );
+                }
+            } elseif ($container->has($rp_name)) {
+                $args[] = $container->get($rp_name);
+            } elseif ($rp->isDefaultValueAvailable()) {
+                $args[] = $rp->getDefaultValue();
             } else {
-                throw new RuntimeException(sprintf(
-                    "Cannot build the object of class `{$fqcn}`, parameter `%s`"
-                    . " is not a dependency FQCN!",
-                    $param->getName()
-                ));
+                throw new RuntimeException(
+                    "Cannot build the object of class `{$fqcn}`:"
+                    . " argument `{$rp_name}` cannot be resolved!"
+                );
             }
         }
 
