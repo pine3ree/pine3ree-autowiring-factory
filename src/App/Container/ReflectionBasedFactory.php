5ea9499019b7422d9cc1629e7eb8710a967d5dcd
pine3ree <pine3ree@gmail.com>
2025-04-24 01:04:12 +0200

    Make reflection-based factory use the params-resolver in order to avoid code duplication
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/App/Container/ReflectionBasedFactory.php
+++ src/App/Container/ReflectionBasedFactory.php
@@ -48,56 +48,56 @@ class ReflectionBasedFactory
         $args = $paramsResolver->resolve([$fqcn, '__construct']);
 
         return empty($args) ? new $fqcn() : new $fqcn(...$args);
-
-        $rc = $this->getReflectionClass($fqcn);
-        $__r_params = $this->constructorParams[$fqcn];
-
-        // Parameter-less object constructor or consructor-less object
-        if (empty($__r_params)) {
-            return new $fqcn();
-        }
-
-        $args = [];
-        foreach ($__r_params as $rp) {
-            $rp_name = $rp->getName();
-            $rp_type = $rp->getType();
-            if ($rp_type instanceof ReflectionNamedType && !$rp_type->isBuiltin()) {
-                $rp_fqcn = $rp_type->getName();
-                if (interface_exists($rp_fqcn, true) || class_exists($rp_fqcn, true)) {
-                    if ($rp_fqcn === ContainerInterface::class || $rp_fqcn === get_class($container)) {
-                        $args[] = $container;
-                    } elseif ($container->has($rp_fqcn)) {
-                        $args[] = $container->get($rp_fqcn);
-                    } else {
-                        // Try instantating with argument-less constructor call
-                        try {
-                            $args[] = new $rp_fqcn();
-                        } catch (Throwable $ex) {
-                            throw new RuntimeException(
-                                "Unable to create an instance of `{$rp_fqcn}`"
-                                . " for the argument with name `{$rp_name}`!",
-                            );
-                        }
-                    }
-                } else {
-                    throw new RuntimeException(
-                        "`{$rp_fqcn}` is neither a valid interface nor a class name"
-                        . " for given dependency named `{$rp_name}`!",
-                    );
-                }
-            } elseif ($container->has($rp_name)) {
-                $args[] = $container->get($rp_name);
-            } elseif ($rp->isDefaultValueAvailable()) {
-                $args[] = $rp->getDefaultValue();
-            } else {
-                throw new RuntimeException(
-                    "Cannot build the object of class `{$fqcn}`:"
-                    . " argument `{$rp_name}` cannot be resolved!"
-                );
-            }
-        }
-
-        return $rc->newInstanceArgs($args);
+//
+//        $rc = $this->getReflectionClass($fqcn);
+//        $__r_params = $this->constructorParams[$fqcn];
+//
+//        // Parameter-less object constructor or consructor-less object
+//        if (empty($__r_params)) {
+//            return new $fqcn();
+//        }
+//
+//        $args = [];
+//        foreach ($__r_params as $rp) {
+//            $rp_name = $rp->getName();
+//            $rp_type = $rp->getType();
+//            if ($rp_type instanceof ReflectionNamedType && !$rp_type->isBuiltin()) {
+//                $rp_fqcn = $rp_type->getName();
+//                if (interface_exists($rp_fqcn, true) || class_exists($rp_fqcn, true)) {
+//                    if ($rp_fqcn === ContainerInterface::class || $rp_fqcn === get_class($container)) {
+//                        $args[] = $container;
+//                    } elseif ($container->has($rp_fqcn)) {
+//                        $args[] = $container->get($rp_fqcn);
+//                    } else {
+//                        // Try instantating with argument-less constructor call
+//                        try {
+//                            $args[] = new $rp_fqcn();
+//                        } catch (Throwable $ex) {
+//                            throw new RuntimeException(
+//                                "Unable to create an instance of `{$rp_fqcn}`"
+//                                . " for the argument with name `{$rp_name}`!",
+//                            );
+//                        }
+//                    }
+//                } else {
+//                    throw new RuntimeException(
+//                        "`{$rp_fqcn}` is neither a valid interface nor a class name"
+//                        . " for given dependency named `{$rp_name}`!",
+//                    );
+//                }
+//            } elseif ($container->has($rp_name)) {
+//                $args[] = $container->get($rp_name);
+//            } elseif ($rp->isDefaultValueAvailable()) {
+//                $args[] = $rp->getDefaultValue();
+//            } else {
+//                throw new RuntimeException(
+//                    "Cannot build the object of class `{$fqcn}`:"
+//                    . " argument `{$rp_name}` cannot be resolved!"
+//                );
+//            }
+//        }
+//
+//        return $rc->newInstanceArgs($args);
     }
 
     /**
