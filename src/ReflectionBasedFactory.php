953b41909dfe3200402c3d64e46a30de91a1467f
pine3ree <pine3ree@gmail.com>
2025-04-28 02:18:15 +0200

    Cache params-resolver for provided constructors instances using SplObjectStorage
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/ReflectionBasedFactory.php
+++ src/ReflectionBasedFactory.php
@@ -10,24 +10,47 @@ namespace pine3ree\Container;
 use Psr\Container\ContainerInterface;
 use pine3ree\Container\ParamsResolver;
 use pine3ree\Container\ParamsResolverInterface;
+use RuntimeException;
+use SplObjectStorage;
+use Throwable;
+
+use function method_exists;
 
 /**
  * A generic factory that resolves and injects dependencies using reflection
  */
 class ReflectionBasedFactory
 {
+    private SplObjectStorage $storage;
+
+    public function __construct()
+    {
+        $this->storage = new SplObjectStorage();
+    }
+
     public function __invoke(ContainerInterface $container, string $fqcn): object
     {
-        $has_constructor = method_exists($fqcn, '__construct');
-
-        if (!$has_constructor) {
-            return new $fqcn();
+        if (!method_exists($fqcn, '__construct')) {
+            try {
+                return new $fqcn();
+            } catch (Throwable $ex) {
+                throw new RuntimeException(
+                    "Unable ti instantiate a constructor-less object of class `{$fqcn}`"
+                );
+            }
         }
 
-        $paramsResolver
-            = $container->has(ParamsResolverInterface::class)
-            ? $container->get(ParamsResolverInterface::class)
-            : new ParamsResolver($container);
+        $storage = $this->storage;
+        if ($storage->offsetExists($container)) {
+            $paramsResolver = $storage->offsetGet($container);
+        } else {
+            if ($container->has(ParamsResolverInterface::class)) {
+                $paramsResolver = $container->get(ParamsResolverInterface::class);
+            } else {
+                $paramsResolver = new ParamsResolver($container);
+            }
+            $storage->attach($container, $paramsResolver);
+        }
 
         $args = $paramsResolver->resolve([$fqcn, '__construct']);
 
